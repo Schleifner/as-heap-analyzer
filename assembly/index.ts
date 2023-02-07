c6038eb21f78b35c3002db6ec2cc1f5ca4f443fb
Congcong Cai <congcong.ca.cai@bmw.com>
2023-02-07 11:55:52 +0800

    fix: as use heap data replace map, fix ts/as heap block spilt incorrectly (#11)

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,34 +1,36 @@
-export const memoryDetail = new Map<i32, i32>();
+const MEMORY_DETAIL_SIZE: u32 = isDefined(PREDEFINED_MEMORY_DETAIL_SIZE) ? PREDEFINED_MEMORY_DETAIL_SIZE : 1024;
+export const memoryDetail: u32 = u32(memory.data(MEMORY_DETAIL_SIZE * 4));
 
-export function getUsedMemorySize(): i32 {
-  memoryDetail.clear();
-  const rootOffset = (u32(__heap_base) + 15) & ~15;
-  const memStart = ((rootOffset + 1572 + 4 + 15) & ~15) - 4;
-  let result = memStart;
-  let next = memStart;
-  while (next < u32(memory.size()) * 64 * 1024 - 4) {
-    const currentOffset = next;
-    const memoryInfo = load<u32>(currentOffset);
-    next += 4 + memoryInfo;
-    const blockSize = (4 + memoryInfo) & ~3;
-    if (blockSize <= 12) {
+export function getUsedMemorySize(): u32 {
+  __collect();
+  memory.fill(memoryDetail, 0, MEMORY_DETAIL_SIZE * 4);
+  const rootOffset: u32 = (u32(__heap_base) + 15) & ~15;
+  const memStart: u32 = ((rootOffset + 1572 + 4 + 15) & ~15) - 4;
+  const totalMemory: u32 = u32(memory.size()) * 64 * 1024;
+  let result: u32 = memStart;
+  let next: u32 = memStart;
+  while (next < totalMemory - 4) {
+    const currentOffset: u32 = next;
+    const memoryInfo: u32 = load<u32>(currentOffset);
+    const blockSize: u32 = (4 + memoryInfo) & ~3;
+    next += blockSize;
+    if (blockSize < 16 || currentOffset + blockSize >= totalMemory) {
       return -1;
     }
-    if ((memoryInfo & 1) == 0) {
-      result += blockSize;
+    if ((memoryInfo & 1) == 1) {
+      continue;
     }
-    const objectSize = load<u32>(currentOffset, 4 * 4);
+    result += blockSize;
+    const objectSize: u32 = load<u32>(currentOffset, 4 * 4);
     if (objectSize + 4 * 4 > memoryInfo) {
       continue;
     }
     if (load<u32>(currentOffset, 4) == 0 && load<u32>(currentOffset, 2 * 4) == 0) {
       continue;
     }
-    const runtimeId = load<u32>(currentOffset, 3 * 4);
-    if (memoryDetail.has(runtimeId)) {
-      memoryDetail.set(runtimeId, memoryDetail.get(runtimeId) + blockSize);
-    } else {
-      memoryDetail.set(runtimeId, blockSize);
+    const runtimeId: u32 = load<u32>(currentOffset, 3 * 4);
+    if (runtimeId < MEMORY_DETAIL_SIZE) {
+      store<u32>(memoryDetail + runtimeId * 4, load<u32>(memoryDetail + runtimeId * 4) + blockSize);
     }
   }
   return result;
@@ -36,11 +38,15 @@ export function getUsedMemorySize(): i32 {
 
 export function dumpUsedMemoryDetail(): string {
   const totalUsage = getUsedMemorySize();
-  let result = `totalUsage: ${totalUsage}\n`;
-  const k = memoryDetail.keys();
-  const v = memoryDetail.values();
-  for (let i: i32 = 0; i < k.length; i++) {
-    result += `class id ${k[i]} uses ${v[i]} bytes memory\n`;
+  let result = `{\n\ttotalUsage: ${totalUsage},\n\tclassDetail: {\n`;
+  let classDetail: string[] = [];
+  for (let i: u32 = 0; i < MEMORY_DETAIL_SIZE; i++) {
+    const v = load<u32>(memoryDetail + i * 4);
+    if (v != 0) {
+      classDetail.push(`\t\t"${i}": ${v}`);
+    }
   }
+  result += classDetail.join(",\n");
+  result += `\n\t}\n}`;
   return result;
 }
